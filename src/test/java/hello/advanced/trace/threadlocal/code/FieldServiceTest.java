0ae1ec34faa23251b59f2e0ecbcc7ecb264304e1
yeojin822 <xlzt2002@gmail.com>
2023-02-17 23:24:47 +0900

    feat : V2, V3 로그분석기

---
+++ src/test/java/hello/advanced/trace/threadlocal/code/FieldServiceTest.java
@@ -0,0 +1,49 @@
+package hello.advanced.trace.threadlocal.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field() {
+        log.info("main start");
+
+//        Runnable userA = new Runnable() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        }
+        // 위 코드를 람다로 표현
+
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000); // 동시성 문제 발생 X
+        sleep(100); // 동시성 문제 발생 O
+        threadB.start();
+        sleep(3000); // 메인 쓰레드 종료 대기
+        log.info("main exit");
+    }
+
+    private void sleep(int i) {
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
